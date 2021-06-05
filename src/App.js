bb7549ffe18427cd9167773ae41e42ef3782e18c
vangona <rlarhksrud12@naver.com>
2021-06-05 16:55:59 +0900

    2021.06.05 commit
    - nomadcoder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return (<HashRouter>
+  return (
+  <HashRouter>
+    <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/movie-detail" component={Detail} />
   </HashRouter>
   )
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./About.css";
 
 function About(){
-    return <span>About this page: I built it because I love movies.</span>
+    return (
+            <div class="about__container">
+                <span>
+                    About this page: I built it because I love movies.
+                </span>
+            </div>
+    )
 }
 
 export default About;
